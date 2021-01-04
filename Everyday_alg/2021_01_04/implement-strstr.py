ab2f7c9c700a2f2fa8e48c83afedbd7f27769f1a
Alexanderklau <429095816@qq.com>
2021-01-04 19:01:43 +0800

    add

---
+++ Everyday_alg/2021_01_04/implement-strstr.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+__author__ = 'Yemilice_lau'
+
+
+"""
+28. 实现 strStr()
+实现 strStr() 函数。
+
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1:
+
+输入: haystack = "hello", needle = "ll"
+输出: 2
+示例 2:
+
+输入: haystack = "aaaaa", needle = "bba"
+输出: -1
+"""
+
+"""
+看起来只能循环
+"""
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
+
+
