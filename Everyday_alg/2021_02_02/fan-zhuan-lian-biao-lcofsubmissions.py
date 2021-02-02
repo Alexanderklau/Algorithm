9319f7cc8b29ba16909656d087ea2110dd6d1e67
Alexanderklau <429095816@qq.com>
2021-02-02 22:26:01 +0800

    add

---
+++ Everyday_alg/2021_02_02/fan-zhuan-lian-biao-lcofsubmissions.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+ 
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+ 
+
+限制：
+
+0 <= 节点个数 <= 5000
+
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        z = ListNode
+        next = None
+        while head:
+            nextNode = head.next
+            head.next = next
+            next = head
+            head = nextNode
+        return next
