2b34d49cfcdaecb7373b8d48de26eecb34bdc0fd
Alexanderklau <429095816@qq.com>
2021-03-04 00:17:02 +0800

    add

---
+++ Everyday_alg/2021_03_05/single-element-in-a-sorted-array.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+给定一个只包含整数的有序数组，每个元素都会出现两次，唯有一个数只会出现一次，找出这个数。
+
+示例 1:
+
+输入: [1,1,2,3,3,4,4,8,8]
+输出: 2
+示例 2:
+
+输入: [3,3,7,7,10,11,11]
+输出: 10
+"""
+
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            halves_are_even = (hi - mid) % 2 == 0
+            if nums[mid + 1] == nums[mid]:
+                if halves_are_even:
+                    lo = mid + 2
+                else:
+                    hi = mid - 1
+            elif nums[mid - 1] == nums[mid]:
+                if halves_are_even:
+                    hi = mid - 2
+                else:
+                    lo = mid + 1
+            else:
+                return nums[mid]
+        return nums[lo]
