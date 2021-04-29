92a0bfe4d87bf073cf52f748a2cd85db28eab5c2
Alexanderklau <429095816@qq.com>
2021-04-30 00:56:30 +0800

    add

---
+++ Everyday_alg/2021_04_30/search-insert-position.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+
+输入: [1,3,5,6], 0
+输出: 0
+"""
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                return mid
+        return low
