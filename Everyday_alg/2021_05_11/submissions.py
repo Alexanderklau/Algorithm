3fccc7cee895c5efaa064205c14775f6887443d9
Alexanderklau <429095816@qq.com>
2021-05-11 23:24:02 +0800

    add

---
+++ Everyday_alg/2021_05_11/submissions.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+"""
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+
+        if l1 is None:
+            return l2
+
+        if l2 is None:
+            return l1
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
