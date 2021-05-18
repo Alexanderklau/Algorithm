c7ffa36e63db72a142c0ac9e293cad741652c8da
Alexanderklau <429095816@qq.com>
2021-05-18 21:11:04 +0800

    add tree

---
+++ Everyday_alg/2021_05_18/binary-tree-maximum-path-sum.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+
+"""
+径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+
+路径和 是路径中各节点值的总和。
+
+给你一个二叉树的根节点 root ，返回其 最大路径和 。
+
+
+输入：root = [1,2,3]
+输出：6
+解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+示例 2：
+
+
+输入：root = [-10,9,20,null,null,15,7]
+输出：42
+解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def __init__(self):
+        self.ans = float("-inf")
+
+    def maxPathSum(self, root):
+        def onemax(root):
+            if root == None:
+                return 0
+
+            left = max(0, onemax(root.left))
+            right = max(0, onemax(root.right))
+
+            self.ans = max(self.ans, right + left + root.val)
+
+            return max(left, right) + root.val
+        onemax(root)
+        return self.ans
+
+
