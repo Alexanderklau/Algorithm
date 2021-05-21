d6d4daa52efebbf522c87599606a38f84a814a7e
Alexanderklau <429095816@qq.com>
2021-05-21 20:57:28 +0800

    add

---
+++ Everyday_alg/2021_05_21/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+进阶：
+
+你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ 
+
+示例 1：
+
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+"""
+
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] == target and nums[right] == target:
+                return [left, right]
+            elif nums[left] < target:
+                left += 1
+            elif nums[right] > target:
+                right -= 1
+
+        return [-1, -1]
