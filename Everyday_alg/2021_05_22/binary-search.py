c33579ac55ff0f0dedbf56631c5e39f09ecf6d8e
Alexanderklau <429095816@qq.com>
2021-05-22 22:39:20 +0800

    add

---
+++ Everyday_alg/2021_05_22/binary-search.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+示例 1:
+
+输入: nums = [-1,0,3,5,9,12], target = 9
+输出: 4
+解释: 9 出现在 nums 中并且下标为 4
+示例 2:
+
+输入: nums = [-1,0,3,5,9,12], target = 2
+输出: -1
+解释: 2 不存在 nums 中因此返回 -1
+
+"""
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left += 1
+            elif nums[mid] > target:
+                right -= 1
+            elif nums[mid] == target:
+                return mid
+        return - 1
