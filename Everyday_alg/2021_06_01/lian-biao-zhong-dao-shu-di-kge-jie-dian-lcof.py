77b0cfee57a6b3f865dbf9d1d74691ada3e7fe5e
Alexanderklau <429095816@qq.com>
2021-06-01 22:14:16 +0800

    add

---
+++ Everyday_alg/2021_06_01/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+
+ 
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+
+返回链表 4->5.
+"""
+class Solution(object):
+    def getKthFromEnd(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        z = head
+        n = head
+        step = 1
+
+        while z.next:
+            if step == k:
+                n = n.next
+            else:
+                step += 1
+            z = z.next
+        return n
