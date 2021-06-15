684cae126f998bdae491800bdce47a84f88814e2
Alexanderklau <429095816@qq.com>
2021-06-15 23:11:41 +0800

    add

---
+++ Everyday_alg/2021_06_15/zhong-jian-er-cha-shu-lcof.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+
+ 
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ 
+
+限制：
+
+0 <= 节点个数 <= 5000
+"""
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        stack = [root]
+        inorderIndex = 0
+        for i in range(1, len(preorder)):
+            preorderVal = preorder[i]
+            node = stack[-1]
+            if node.val != inorder[inorderIndex]:
+                node.left = TreeNode(preorderVal)
+                stack.append(node.left)
+            else:
+                while stack and stack[-1].val == inorder[inorderIndex]:
+                    node = stack.pop()
+                    inorderIndex += 1
+                node.right = TreeNode(preorderVal)
+                stack.append(node.right)
+
+        return root
