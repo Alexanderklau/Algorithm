daae679109c64181e04d374b2e524e31bfecce6d
Alexanderklau <429095816@qq.com>
2021-06-28 23:20:03 +0800

    add

---
+++ HOT100/daily-temperatures.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+__author__ = "lau.wenbo"
+
+"""
+请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
+例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+"""
+
+
+class Solution(object):
+    def __init__(self):
+        self.maxday = 0
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        nums = len(temperatures)
+
+        ret = [0] * nums
+
+        stack = []
+
+        for i in range(nums):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                index = stack.pop()
+                ret[index] = i - index
+            stack.append(i)
+        return ret
