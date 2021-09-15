aba1822c8da24974048204d705824c1f97aa0bda
Alexanderklau <429095816@qq.com>
2021-09-15 15:59:37 +0800

    fix

---
+++ Everyday_alg/2021_09_15/longest-increasing-subsequence.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+__author__ = 'Yemilice_lau'
+
+"""
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+
+子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+
+例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+示例 1：
+
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+示例 2：
+
+输入：nums = [0,1,0,3,2,3]
+输出：4
+示例 3：
+
+输入：nums = [7,7,7,7,7,7,7]
+输出：1
+"""
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return max(self.dpwork(nums))
+
+    def dpwork(self, arr):
+        n = len(arr)
+        dp = [0] * n
+        for i in range(n):
+            dp[i] = 1
+            for j in range(i):
+                if arr[i] > arr[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return dp
