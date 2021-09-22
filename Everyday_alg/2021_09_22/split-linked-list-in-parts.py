bfb17a7b528d936a09f813352d85c2e8c1d05c5d
Alexanderklau <429095816@qq.com>
2021-09-22 12:53:19 +0800

    fix

---
+++ Everyday_alg/2021_09_22/split-linked-list-in-parts.py
@@ -0,0 +1,67 @@
+# coding: utf-8
+
+__author__ = 'Yemilice_lau'
+
+
+"""
+给你一个头结点为 head 的单链表和一个整数 k ，请你设计一个算法将链表分隔为 k 个连续的部分。
+
+每部分的长度应该尽可能的相等：任意两部分的长度差距不能超过 1 。这可能会导致有些部分为 null 。
+
+这 k 个部分应该按照在链表中出现的顺序排列，并且排在前面的部分的长度应该大于或等于排在后面的长度。
+
+返回一个由上述 k 部分组成的数组。
+
+ 
+示例 1：
+输入：head = [1,2,3], k = 5
+输出：[[1],[2],[3],[],[]]
+解释：
+第一个元素 output[0] 为 output[0].val = 1 ，output[0].next = null 。
+最后一个元素 output[4] 为 null ，但它作为 ListNode 的字符串表示是 [] 。
+
+示例 2：
+输入：head = [1,2,3,4,5,6,7,8,9,10], k = 3
+输出：[[1,2,3,4],[5,6,7],[8,9,10]]
+解释：
+输入被分成了几个连续的部分，并且每部分的长度相差不超过 1 。前面部分的长度大于等于后面部分的长度。
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def splitListToParts(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: List[ListNode]
+        """
+        nums = self.length(head)
+        sort = nums // k
+        l = nums % k
+
+        ans = []
+        pre, cur = None, head
+        for _ in range(k):
+            ans.append(cur)
+            tmp = sort + 1 if l > 0 else sort
+            for _ in range(tmp):
+                pre = cur
+                cur = cur.next
+            if pre:
+                pre.next = None
+            if l > 0:
+                l -= 1
+        return ans
+
+    def length(self, head):
+        n = 0
+        cur = head
+        while cur:
+            n += 1
+            cur = cur.next
+        return n
