bbeac85fddeaa8ff8dbedb00062119b520b89f8e
Klichko Yemilice <429095816@qq.com>
2021-11-11 23:10:45 +0800

    add

---
+++ Everyday_alg/2021/11/2021_11_11/k-inverse-pairs-array.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+__author__ = 'Yemilice'
+
+
+"""
+给出两个整数 n 和 k，找出所有包含从 1 到 n 的数字，且恰好拥有 k 个逆序对的不同的数组的个数。
+
+逆序对的定义如下：对于数组的第i个和第 j个元素，如果满i < j且 a[i] > a[j]，则其为一个逆序对；否则不是。
+
+由于答案可能很大，只需要返回 答案 mod 109 + 7 的值。
+
+示例 1:
+
+输入: n = 3, k = 0
+输出: 1
+解释: 
+只有数组 [1,2,3] 包含了从1到3的整数并且正好拥有 0 个逆序对。
+示例 2:
+
+输入: n = 3, k = 1
+输出: 2
+解释: 
+数组 [1,3,2] 和 [2,1,3] 都有 1 个逆序对。
+"""
+
+class Solution(object):
+    def kInversePairs(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        mod = 10 ** 9 + 7
+
+        f = [1] + [0] * k
+        for i in range(1, n + 1):
+            g = [0] * (k + 1)
+            for j in range(k + 1):
+                g[j] = (g[j - 1] if j - 1 >= 0 else 0) - (f[j - i] if j - i >= 0 else 0) + f[j]
+                g[j] %= mod
+            f = g
+
+        return f[k]
