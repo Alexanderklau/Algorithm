9626ebe8c3a2efbc25c700470d1ce64168f14bba
Alexanderklau <429095816@qq.com>
2021-12-07 13:47:39 +0800

    island

---
+++ Everyday_alg/2021/12/2021_12_07/number-of-islands.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+__author__ = 'Yemilice_lau'
+
+
+"""
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+
+此外，你可以假设该网格的四条边均被水包围。
+
+ 
+
+示例 1：
+
+输入：grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+输出：1
+示例 2：
+
+输入：grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+输出：3
+"""
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        count = 0
+        y = len(grid)
+        x = len(grid[0])
+        for i in range(y):
+            for j in range(x):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if not 0 <= i < len(grid) or not 0 <= j < len(grid[0]) or grid[i][j] == '0':
+            return
+        else:
+            grid[i][j] = '0'
+            self.dfs(grid, i - 1, j)
+            self.dfs(grid, i + 1, j)
+            self.dfs(grid, i, j - 1)
+            self.dfs(grid, i, j + 1)
