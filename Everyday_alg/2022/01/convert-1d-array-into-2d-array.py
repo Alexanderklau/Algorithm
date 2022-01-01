3d8e7543cca9b7bc17d81747f84341acbc685fca
Alexanderklau <429095816@qq.com>
2022-01-01 14:40:50 +0800

    add

---
+++ Everyday_alg/2022/01/convert-1d-array-into-2d-array.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+__author__ = 'Yemilice_lau'
+
+
+"""
+给你一个下标从 0 开始的一维整数数组 original 和两个整数 m 和  n 。
+
+
+你需要使用 original 中 所有 元素创建一个 m 行 n 列的二维数组。
+
+original 中下标从 0 到 n - 1 （都 包含 ）的元素构成二维数组的第一行，
+
+
+下标从 n 到 2 * n - 1 （都 包含 ）的元素构成二维数组的第二行，依此类推。
+
+请你根据上述过程返回一个 m x n 的二维数组。如果无法构成这样的二维数组，请你返回一个空的二维数组。
+
+输入：original = [1,2,3,4], m = 2, n = 2
+输出：[[1,2],[3,4]]
+解释：
+构造出的二维数组应该包含 2 行 2 列。
+original 中第一个 n=2 的部分为 [1,2] ，构成二维数组的第一行。
+original 中第二个 n=2 的部分为 [3,4] ，构成二维数组的第二行。
+
+输入：original = [3], m = 1, n = 2
+输出：[]
+解释：
+original 中只有 1 个元素。
+无法将 1 个元素放满一个 1x2 的二维数组，所以返回一个空的二维数组。
+"""
+
+
+class Solution(object):
+    def construct2DArray(self, original, m, n):
+        """
+        :type original: List[int]
+        :type m: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        if len(original) != (m * n):
+            return []
+
+        res = []
+        for i in range(0, len(original), n):
+            res.append(original[i:i+n])
+
+        return res
+
+
+
