b14762acb82bc842546be8a562c55c6e85b05635
Klichko Yemilice <429095816@qq.com>
2024-08-14 23:46:38 +0800

    2024/08/14 update

---
+++ Everyday_alg/2024/2024-08-14/special_arrays_2.py
@@ -0,0 +1,22 @@
+'''
+ # @ Author: Yemilice_lau
+ # @ Create Time: 2024-08-14 23:02:04
+ # @ Modified by: Yemilice_lau
+ # @ Modified time: 2024-08-14 23:31:04
+ # @ Description: This is leetcode everyday_alg: isArraySpecial
+ '''
+
+class Solution:
+   def searchBreakPoints(self, nums: List[int]) -> List[int]:
+       breakPoints = [i for i in range(len(nums))]
+       for i in range(len(nums)-1, 0, -1):
+           if (nums[i - 1] + nums[i]) % 2 == 1:
+               breakPoints[i - 1] = breakPoints[i]
+       return breakPoints
+
+   def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+       breakPoints = self.searchBreakPoints(nums)
+       answer = []
+       for start, end in queries:
+           answer.append(breakPoints[start] >= end)
+       return answer
